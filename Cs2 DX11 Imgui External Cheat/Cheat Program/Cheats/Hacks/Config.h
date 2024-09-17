cd0334350e2cab87fc7759356382b126afec569e
Neo7 <164035951+NeoXa7@users.noreply.github.com>
2024-09-17 13:00:26 +0545

    Add files via upload

---
+++ Cs2 DX11 Imgui External Cheat/Cheat Program/Cheats/Hacks/Config.h
@@ -0,0 +1,236 @@
+#pragma once
+#include <Maths/hColor.h>
+#include <Maths/hVector.h>
+#include <Hacks/Bones.h>
+
+Color color;
+
+namespace Config
+{
+	namespace Game
+	{
+		ImColor TerroristColor = C_Elements::RGBToImColor(234, 209, 139);
+		ImColor CounterTerroristColor = C_Elements::RGBToImColor(182, 212, 238);	
+		ImVec4 SpectatorColor = ImVec4(255.0f / 255.0f, 255.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f);
+	}
+
+	namespace TriggerBot
+	{
+		inline bool enableHotKey = true;
+		inline bool enableTeamCheck = true;
+		static int TB_KEYCODE = 0xA0;
+		static char TB_Default_KEYCODE_STR[16] = "A0";
+	}
+
+	namespace RCS
+	{
+		float oldPunch_x = 0.0f;
+		float oldPunch_y = 0.0f;
+	}
+
+	namespace GlowHack
+	{
+		const char* AvailableColors[] = { "Green", "Yellow", "Blue", "Red", "White", "Purple", "Cyan", "LightBlue" };
+		static int CurrentColorIndex = 0;
+
+		namespace GlowColor
+		{
+			DWORD64 WHITE = color.convertToARGB(Color(253, 228, 132, 255));
+			DWORD64 BLUE = color.convertToARGB(Color(128, 0, 0, 255));
+			DWORD64 RED = color.convertToARGB(Color(0, 0, 255, 255));
+			DWORD64 PURPLE = color.convertToARGB(Color(128, 0, 128, 255));
+			DWORD64 GREEN = color.convertToARGB(Color(0, 255, 0, 255));
+			DWORD64 CYAN = color.convertToARGB(Color(255, 255, 0, 255));
+			DWORD64 LIGHTBLUE = color.convertToARGB(Color(0, 0, 0, 255));
+			DWORD64 YELLOW = color.convertToARGB(Color::fromHSV(180.0f, 1.0f, 1.0f));
+
+			DWORD64 CurrentGlowColor = GREEN;
+		}
+
+		namespace GlowSettings
+		{
+			inline bool TeamGlow = false;
+			inline bool DeadEntityGlow = false;
+		}
+
+	}
+
+	namespace Esp
+	{	
+		inline bool enableEspBoxes = true;
+		inline bool enableEspLines = false;
+
+		inline bool enableEspHealthBar = true;
+		inline bool enableEspHealthPointsText = true;
+
+		inline bool enableEspArmorBar = true;
+		inline bool enableEspArmorPointsText = true;
+
+		inline bool enableEspNames = true;
+		inline bool enableTeamCheck = true;
+		inline bool enableEspBones = true;
+		inline bool headBoneFilled = false;
+
+		ImColor BoxColor = ImColor(255, 255, 255, 255); // Default white;
+		ImColor HealthBarColor = ImColor(0, 255, 0, 255); // Default green;
+		ImColor ArmorBarColor = C_Elements::RGBToImColor(182, 212, 238);
+		ImColor LinesColor = ImColor(255, 255, 255, 255); // Default white;
+		ImColor NameColor = ImColor(255, 255, 255, 255); // Default white;
+		ImColor LineEndPointCircleColor = ImColor(255, 255, 255, 255); // Default white
+		ImColor BonesColor = ImColor(255, 255, 255, 255); // Default white;
+		ImColor HeadBoneColor = ImColor(255, 255, 255, 255); // Default white;
+
+		float BonesThickness = 0.5f;
+		float LinesThickness = 1.5f;
+		float Lines_EndPoint_Radius = 3.0f;
+		float BoxThickness = 1.0f;
+
+		const char* availableSnapLinesPos[] = { "Top", "Center", "Bottom"};
+		static int currentlinePosIndex = 2;
+		Vector2 currentlinePos;
+
+		Vector3 SetBoneCoordinates(uintptr_t boneMatrix, BoneIndex bone)
+		{
+			return Memory::ReadMemory<Vector3>(boneMatrix + (bone * 0x20));
+		}
+
+	}
+
+	namespace Aimbot
+	{
+		inline bool enableHotKey = false;
+		static int AB_KEYCODE = 0x06;
+		static char AB_Default_KEYCODE_STR[16] = "06";
+
+		const char* AimPositions[] = {
+			"Head",
+			"Neck",
+			"Chest",
+			"Stomach",
+			"Penis",
+			"Left Shoulder",
+			"Left Arm",
+			"Left Hand",
+			"Right Shoulder",
+			"Right Arm",
+			"Right Hand",
+			"Left Thigh",
+			"Left Calf",
+			"Left Foot",
+			"Right Thigh",
+			"Right Calf",
+			"Right Foot"
+		};
+		static int CurrentAimPosIndex; // Index of the currently selected item
+
+		static float AimFov = 10.f;
+		static float AimSmoothness = 1.f;
+
+		static bool ShowFovCircle = false;
+		static ImVec4 FovCircleColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); // Default White;
+		static float FovCircleThickness = 1.5f;	
+		inline bool TeamCheck = true;
+
+		Vector3 SetPenisCoordinates(Vector3 entityPosition, uintptr_t entityPawn)
+		{
+			return entityPosition + Memory::ReadMemory<Vector3>(entityPawn + Offsets::m_vecViewOffset) * 0.5;
+		}
+
+		Vector3 SetHeadCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::HEAD * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetNeckCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::NECK * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetChestCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::CHEST * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetStomachCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::STOMACH * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLShoulderCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LSHOULDER * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLArmCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LARM * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLHandCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LHAND * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRShoulderCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RSHOULDER * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRArmCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RARM * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRHandCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RHAND * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLThighCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LTHIGH * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLCalfCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LCALF * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetLFootCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::LFOOT * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRThighCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RTHIGH * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRCalfCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RCALF * 0x20));
+			return entityPosition;
+		}
+
+		Vector3 SetRFootCoordinates(uintptr_t boneMatrix)
+		{
+			Vector3 entityPosition = Memory::ReadMemory<Vector3>(boneMatrix + (BoneIndex::RFOOT * 0x20));
+			return entityPosition;
+		}
+
+	}
+}
---
+++ Cs2 DX11 Imgui External Cheat/Cheat Program/Cheats/Readers/Game.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <Memory/hMemory.h>
+#include <Maths/hVector.h>
+#include <Globals/hGlobals.h>
+#include <Maths/ViewMatrix.h>
+
+class Game
+{
+public:
+	uintptr_t EntityList;
+	uintptr_t Rules;
+public:
+	char MapName[128] = { 0 };
+public:
+	bool BombPlanted;
+	bool BombDropped;
+	int BombSite;
+	bool IsBombDefusing;
+public:
+	float FlashDuration;
+	int EntityIndex;
+public:
+	Vector3 ViewAngles;
+	view_matrix_t ViewMatrix;
+public:
+	void UpdateGameVars()
+	{
+		EntityList = Memory::ReadMemory<uintptr_t>(CS2::ClientDll + Offsets::dwEntityList);
+		Rules = Memory::ReadMemory<uintptr_t>(CS2::ClientDll + Offsets::dwGameRules);
+
+		uintptr_t MapNameAddress = Memory::ReadMemory<uintptr_t>(CS2::MatchMakingDll + Offsets::dwGameTypes + Offsets::dwGameTypes_mapName + 8);
+		Memory::ReadArray<char>(MapNameAddress, MapName, sizeof(MapName));
+
+		BombPlanted = Memory::ReadMemory<bool>(Rules + Offsets::m_bBombPlanted);
+		BombDropped = Memory::ReadMemory<bool>(Rules + Offsets::m_bBombDropped);
+		BombSite = Memory::ReadMemory<int>(Rules + Offsets::m_nBombSite);
+
+		FlashDuration = Memory::ReadMemory<float>(player.localPlayerPawn + Offsets::m_flFlashBangTime);
+		EntityIndex = Memory::ReadMemory<int>(player.localPlayerPawn + Offsets::m_iIDEntIndex);
+
+		ViewAngles = Memory::ReadMemory<Vector3>(CS2::ClientDll + Offsets::dwViewAngles);	
+		ViewMatrix = Memory::ReadMemory<view_matrix_t>(CS2::ClientDll + Offsets::dwViewMatrix);
+	}
+}; inline Game game;
