6a375a2158731b4522e901dd8d4a336ea7d20a55
Neo7 <164035951+NeoXa7@users.noreply.github.com>
2024-09-26 19:29:57 +0545

    Add files via upload

---
+++ Cs2 External/Cheats/Hacks/Utils/DrawUtils.h
@@ -0,0 +1,138 @@
+#pragma once
+#include <ImGui/imgui.h>
+#include <Maths/hVector.h>
+#include <Maths/WorldToScreen.h>
+#include <Hacks/Utils/Config.h>
+#include <Readers/Entity.h>
+#include <Readers/Game.h>
+
+namespace DrawUtils
+{
+    inline void DrawBox(const Vector2& topLeft, const Vector2& bottomRight, ImColor color = ImColor(255, 255, 255, 255), float thickness = 2.0f) {
+        ImGui::GetBackgroundDrawList()->AddRect(ImVec2(topLeft.x, topLeft.y), ImVec2(bottomRight.x, bottomRight.y), color, 0.0f, ImDrawFlags_None, thickness);
+    }
+
+	inline void DrawLine(const Vector2& from, const Vector2& to, ImColor color = ImColor(255, 255, 255, 255), float thickness = 1.0f, bool drawEndPoint = false, float endPointRadius = 3.0f, ImColor endPointColor = ImColor(255, 255, 255, 255)) {
+		ImGui::GetBackgroundDrawList()->AddLine(ImVec2(from.x, from.y), ImVec2(to.x, to.y), color, thickness);
+
+		if (drawEndPoint) {
+			ImGui::GetBackgroundDrawList()->AddCircleFilled(ImVec2(to.x, to.y), endPointRadius, endPointColor);
+		}
+	}
+
+    inline void DrawHealthBar(const Vector2& topLeft, const Vector2& bottomRight, int health, ImColor fillColor = ImColor(0, 255, 0, 255), float barWidth = 7.0f, float offset = 5.0f) {
+        float boxHeight = bottomRight.y - topLeft.y;
+        float healthHeight = (health / 100.0f) * boxHeight;
+
+        // Draw the filled portion of the health bar (only up to the health amount)
+        ImVec2 healthTopLeft(topLeft.x - barWidth - offset, bottomRight.y - healthHeight); // Adjust based on health
+        ImVec2 healthBottomRight(topLeft.x - offset, bottomRight.y);
+        ImGui::GetBackgroundDrawList()->AddRectFilled(healthTopLeft, healthBottomRight, fillColor, 0.0f, ImDrawFlags_None);
+    }
+
+    inline void DrawTextW(const char* text, const Vector2& position, ImU32 color, bool centered = false)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        ImFont* font = ImGui::GetFont(); // Get the current font
+        ImVec2 textSize = font->CalcTextSizeA(io.FontGlobalScale, FLT_MAX, 0.0f, text); // Get the size of the text
+
+        // Apply global font scale if needed
+        float globalScale = io.FontGlobalScale;
+
+        if (centered)
+        {
+            ImVec2 textPos(position.x - textSize.x * globalScale / 2.0f, position.y - textSize.y * globalScale / 2.0f);
+            ImGui::GetBackgroundDrawList()->AddText(textPos, color, text);
+        }
+        else
+        {
+            ImGui::GetBackgroundDrawList()->AddText(ImVec2(position.x, position.y), color, text);
+        }
+    }
+
+	inline void DrawCircle(float x, float y, float radius, ImColor color = ImColor(255, 255, 255, 255), float thickness = 1.0f, int segments = 0) {
+		ImGui::GetBackgroundDrawList()->AddCircle(ImVec2(x,y), radius, color, segments, thickness);
+	}
+
+    inline void DrawCircleFilled(float x, float y, float radius, ImColor color = ImColor(255, 255, 255, 255)) {
+        ImGui::GetBackgroundDrawList()->AddCircleFilled(ImVec2(x, y), radius, color, 0);
+    }
+
+    inline void DrawBones(uintptr_t pawn)
+    {      
+        uintptr_t BoneMatrix = GetBoneMatrix(pawn);
+        Vector3 head = Memory::ReadMemory<Vector3>(BoneMatrix + BoneIndex::HEAD * 32);
+
+        Vector2 headScreenPos;
+        Vector2 screenPos;
+
+        for (int i = 0; i < sizeof(boneConnections) / sizeof(boneConnections[0]); i++)
+        {
+            int bone1 = boneConnections[i].bone1;
+            int bone2 = boneConnections[i].bone2;
+
+            Vector3 VectorBone1 = Memory::ReadMemory<Vector3>(BoneMatrix + bone1 * 32);
+            Vector3 VectorBone2 = Memory::ReadMemory<Vector3>(BoneMatrix + bone2 * 32);
+
+            if (WorldToScreen(entities->Position, screenPos, game.ViewMatrix) &&
+                WorldToScreen(head, headScreenPos, game.ViewMatrix))
+            {
+                Vector2 b1, b2;
+                if (WorldToScreen(VectorBone1, b1, game.ViewMatrix) &&
+                    WorldToScreen(VectorBone2, b2, game.ViewMatrix))
+                {
+                    float headHeight = entities->GetHeadHeight(screenPos, headScreenPos);
+                    if (!Config::Esp::ShowBoneJoints)
+                    {
+                        DrawUtils::DrawLine(b1, b2, Config::Esp::BonesColor, Config::Esp::BonesThickness);
+                    }
+                    else
+                    {
+                        if (game.Distance_From_LocalPlayer_To_Entities < Config::Esp::JointDisplayDistance)
+                        {
+                            DrawUtils::DrawLine(b1, b2, Config::Esp::BonesColor, Config::Esp::BonesThickness, true, 2.0f, Config::Esp::BonesColor);
+                        }
+                        else
+                        {
+                            DrawUtils::DrawLine(b1, b2, Config::Esp::BonesColor, Config::Esp::BonesThickness);
+                        }
+                            
+                    }
+                    
+
+                    if (!Config::Esp::HeadFilled)
+                    {
+                        DrawUtils::DrawCircle(headScreenPos.x, headScreenPos.y, headHeight, Config::Esp::HeadColor, Config::Esp::BonesThickness);
+                    }
+                    else
+                    {
+                        DrawUtils::DrawCircleFilled(headScreenPos.x, headScreenPos.y, headHeight, Config::Esp::HeadColor);
+                    }
+                    
+                }
+            }          
+        }
+    }
+
+    inline void DrawCrosshair(float crosshairSize = 10.0f, ImColor color = ImColor(255, 255, 255, 255)) {
+        ImDrawList* draw_list = ImGui::GetForegroundDrawList();
+        ImVec2 center = ImVec2(Screen_WIDTH / 2, Screen_HEIGHT / 2);
+
+        // Draw horizontal line
+        draw_list->AddLine(ImVec2(center.x - crosshairSize, center.y),
+            ImVec2(center.x + crosshairSize, center.y), color, 1.0f);
+
+        // Draw vertical line
+        draw_list->AddLine(ImVec2(center.x, center.y - crosshairSize),
+            ImVec2(center.x, center.y + crosshairSize), color, 1.0f);
+    }
+
+    inline void DrawAimFovCircle()
+    {
+        ImDrawList* draw_list = ImGui::GetForegroundDrawList();
+        draw_list->AddCircle(ImVec2(ImGui::GetIO().DisplaySize.x / 2,
+            ImGui::GetIO().DisplaySize.y / 2), Config::Aimbot::AimFov,
+            ImGui::ColorConvertFloat4ToU32(Config::Aimbot::FovCircleColor), 100, Config::Aimbot::FovCircleThickness
+        );
+    }
+}
---
+++ Cs2 External/Cheats/main.cpp
@@ -0,0 +1,307 @@
+
+// Libs 
+#include <ImGui/imgui.h>
+#include <ImGui/imgui_impl_dx11.h>
+#include <ImGui/imgui_impl_win32.h>
+#include <d3d11.h>
+#include <dwmapi.h>
+
+// Internals
+#include <Memory/hMemory.h>
+#include <Memory/hProcess.h>
+#include <Globals/hGlobals.h>
+#include <Menu/ImGuiResourceLoader.h>
+#include <Menu/Interfaces.h>
+#include <Hacks/Hacks.h>
+#include <Functions/hConsole.h>
+#include <Hacks/Utils/Threads.h>
+
+HANDLE Memory::processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, CS2::procID);
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK Window_Procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
+{
+	if (ImGui_ImplWin32_WndProcHandler(window, message, w_param, l_param))
+	{
+		return 0L;
+	}
+
+	if (message == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0L;
+	}
+
+	return DefWindowProc(window, message, w_param, l_param);
+}
+
+INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show)
+{
+	CreateConsoleWindow(L"Console");
+	vector<string> CheatTitle = {
+		"   ___ ___ ___   _____  ___________ ___ _  _   _   _     ",
+		"  / __/ __|_  ) | __\\ \\/ /_   _| __| _ \\ \\| | /_\\ | |    ",
+		" | (__\\__ \\/ /  | _| >  <  | | | _||   / .` |/ _ \\| |__ ",
+		"  \\___|___/___| |___/_/\\_\\ |_| |___|_|_\\_|\\_/_/ \\_\\____|",
+		"                                                       ",
+		"  ______   __  _   _          __  __    _____          ",
+		" | __ ) \\ / / | \\ | | ___  ___\\ \\/ /__ |___  |         ",
+		" |  _ \\\\ V /  |  \\| |/ _ \\/ _ \\\\  // _` | / /          ",
+		" | |_)| | |   | |\\  |  __/ (_) /  \\ (_| |/ /           ",
+		" |____/ |_|   |_| \\_|\\___|\\___/_/\\_\\__,_/_/            ",
+		"                                                       "
+	};
+
+	for (const auto& line : CheatTitle) {
+		SetConsoleTextColor(RED);
+		std::cout << line << std::endl;
+	}
+
+	cout << "  Last Updated 14th September 2024\n";
+
+	if (CS2::procID > 0 && CS2::ClientDll > 0)
+	{
+		SetConsoleTextColor(YELLOW);	
+		cout << '\n';
+		cout << " [+] CS2 Process ID : " << CS2::procID << '\n';
+		cout << '\n';
+		cout << " [+] Retrieving Module Addresess\n";
+		Sleep(500);
+		cout << '\n';
+		cout << "  => Client Address : " << CS2::ClientDll << '\n';
+		cout << "  => Engine2 Address : " << CS2::Engine2Dll << '\n';
+		cout << "  => Inputsystem Address : " << CS2::InputsystemDll << '\n';
+		cout << "  => Matchmaking Address : " << CS2::MatchMakingDll << '\n';
+		cout << "  => Soundsystem Address : " << CS2::SoundsystemDll << '\n';
+		cout << "  => Server Address : " << CS2::ServerDll << '\n';
+		cout << '\n';
+		cout << " [+] Updating Local Player\n";
+		Sleep(500);
+		cout << " [+] Updating Entities\n";
+		Sleep(500);
+		cout << " [+] Updating Game Vars\n";
+		Sleep(500);
+		cout << '\n';
+		cout << " [+] Cheat Attached! \n";
+		CreateLogNotification("Memory", "CONERR$");
+	}
+	else
+	{
+		SetConsoleTextColor(YELLOW);
+		cout << '\n';
+		cout << " [+] CS2 Process ID : " << CS2::procID << '\n';
+		cout << '\n';
+		cout << " [+] Retrieving Module Addresess\n";
+		Sleep(1000);
+		cout << '\n';
+		cout << "  => Client Address : " << CS2::ClientDll << '\n';
+		cout << "  => Engine2 Address : " << CS2::Engine2Dll << '\n';
+		cout << "  => Inputsystem Address : " << CS2::InputsystemDll << '\n';
+		cout << "  => Matchmaking Address : " << CS2::MatchMakingDll << '\n';
+		cout << "  => Soundsystem Address : " << CS2::SoundsystemDll << '\n';
+		cout << "  => Server Address : " << CS2::ServerDll << '\n';
+		cout << '\n';
+		cout << "  Cheat Failed To Attach! \n";
+		CreateLogNotification("Memory", "CONERR$");
+	}
+
+	WNDCLASSEXW wc{};
+	wc.cbSize = sizeof(WNDCLASSEXW);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = Window_Procedure;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = instance;
+	wc.hIcon = 0;
+	wc.hCursor = 0;
+	wc.hbrBackground = 0;
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"External Overlay Class";
+	wc.hIconSm = 0;
+
+	RegisterClassExW(&wc);
+
+	const HWND window = CreateWindowExW(
+		WS_EX_TOPMOST | WS_EX_LAYERED | WS_EX_NOACTIVATE,
+		wc.lpszClassName,
+		L"External Overlay",
+		WS_POPUP,
+		0,
+		0,
+		Screen_WIDTH,
+		Screen_HEIGHT,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr
+	);
+
+
+	SetLayeredWindowAttributes(window, RGB(0, 0, 0), BYTE(255), LWA_COLORKEY);
+
+	{
+		RECT client_area{};
+		GetClientRect(window, &client_area);
+
+		RECT window_area{};
+		GetWindowRect(window, &window_area);
+
+		POINT diff{};
+		ClientToScreen(window, &diff);
+
+		const MARGINS margins{
+			window_area.left + (diff.x - window_area.left),
+			window_area.top + (diff.y - window_area.top),
+			client_area.right,
+			client_area.bottom
+
+		};
+
+		DwmExtendFrameIntoClientArea(window, &margins);
+
+	}
+
+	DXGI_SWAP_CHAIN_DESC sd{};
+	sd.BufferDesc.RefreshRate.Numerator = 60U;
+	sd.BufferDesc.RefreshRate.Denominator = 1U;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.SampleDesc.Count = 1U;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 2U;
+	sd.OutputWindow = window;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	constexpr D3D_FEATURE_LEVEL levels[2]
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_0
+	};
+
+	ID3D11Device* device{ nullptr };
+	ID3D11DeviceContext* device_context{ nullptr };
+	IDXGISwapChain* swap_chain{ nullptr };
+	ID3D11RenderTargetView* render_target_view{ nullptr };
+	D3D_FEATURE_LEVEL level{};
+
+	D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0U,
+		levels,
+		2U,
+		D3D11_SDK_VERSION,
+		&sd,
+		&swap_chain,
+		&device,
+		&level,
+		&device_context
+	);
+
+	ID3D11Texture2D* back_buffer{ nullptr };
+	swap_chain->GetBuffer(0U, IID_PPV_ARGS(&back_buffer));
+
+	if (back_buffer)
+	{
+		device->CreateRenderTargetView(back_buffer, nullptr, &render_target_view);
+		back_buffer->Release();
+	}
+	else
+	{
+		return 1;
+	}
+
+	ShowWindow(window, cmd_show);
+	UpdateWindow(window);
+
+	
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(device, device_context);
+	ImGui_Resource_Loader::LoadStyles(); // Change Fonts Path to Yours In the Imgui Resource Loader in Menu Folder
+	
+	while (Flags::isRunning)
+	{	
+		MSG msg;
+		while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				Flags::isRunning = false;
+			}
+		}
+
+		if (!Flags::isRunning)
+		{
+			break;
+		}
+
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+		// Rendering Starts Here;
+
+		if (CS2::procID > 0)
+		{
+			player.UpdateLocalPlayer();
+			game.UpdateGameVars();	
+
+			CHEATS::ProcessCheatInputs();
+			CHEATS::InitializeBasicCheats();			
+			RenderMenu();
+
+			if (!ProcessManager::IsProcessRunning(CS2::procID))
+			{
+				Flags::isRunning = false;
+			}
+		}
+
+		// Rendering Ends Here;
+		ImGui::Render();
+		constexpr float color[4]{ 0.f, 0.f, 0.f, 0.f };
+		device_context->OMSetRenderTargets(1U, &render_target_view, nullptr);
+		device_context->ClearRenderTargetView(render_target_view, color);
+
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+		swap_chain->Present(0U, 0U);
+	}
+
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	Threaded_Cheats::JoinThreads();
+
+	if (swap_chain)
+	{
+		swap_chain->Release();
+	}
+
+	if (device_context)
+	{
+		device_context->Release();
+	}
+
+	if (device)
+	{
+		device->Release();
+	}
+
+	if (render_target_view)
+	{
+		render_target_view->Release();
+	}
+
+	DestroyWindow(window);
+	UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
+	return 0;
+}
